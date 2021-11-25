8941b40e48f22436bd95fca75646ac72db6475a8
Maryana90 <savkamaryana@ukr.net>
2021-11-25 22:12:31 +0200

    contact form - email address

--- src/components/Contacts/ContactForm/ContactForm.jsx
+++ src/components/Contacts/ContactForm/ContactForm.jsx
@@ -5,7 +5,7 @@ const ContactForm = () => {
   return (
     <div className={q.formBox}>
       <div className={q.innerBox}>
-        <form className={q.form}>
+        <form className={q.form} method="post" action="mailto:savkamaryana@ukr.net">
 
           <div className={q.name + " " + q.formField}>
             <label className={q.labels}>Name</label>
@@ -17,7 +17,7 @@ const ContactForm = () => {
           <div className={q.email + " " + q.formField}>
             <label className={q.labels}>E-mail *</label>
             <div className={q.input}>
-            <input type="text" name="email" />
+            <input type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title="Invalid email address" required />
             </div>
           </div>
 
